9813f275dc5046ebe596ec95f4376e6eec9fd98c
Tomasz Wojdat <tpwojdat@gmail.com>
2025-02-13 00:15:59 +0100

    Redesign the note filename creation process
    Add `created_at` field to the Note dataclass. If title is empty, this
    field is used to serve as a note and filename title. Otherwise the title
    and timestamp are concacinated.
    
    Regex sanitizing title is updated to remove illegal characters instead
    replacing them with `_`, and each space is replaced with `_`, as
    white-space in file names is annoying to handle.
    
    More code in the whole file is updated to correctly handle having title
    and timestamp as separate entities.

--- google_keep_extractor.py
+++ google_keep_extractor.py
@@ -8,6 +8,7 @@ import shutil
 from datetime import datetime
 
 TITLE_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+FILE_TIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 
 IMPORT_PATH = pathlib.Path('Takeout/Keep')
 EXPORT_PATH = pathlib.Path('export')
@@ -20,6 +21,7 @@ JSON_NOTE_LIST = 'listContent'
 @dataclasses.dataclass
 class Note:
     title: str
+    created_at: datetime
     text: str
     attachments: list[str] = dataclasses.field(default_factory=list)
     labels: list[str] = dataclasses.field(default_factory=list)
@@ -28,8 +30,13 @@ class Note:
 def main():
     EXPORT_PATH.mkdir(exist_ok=True)
     for note in _load_notes(IMPORT_PATH):
-        sanitized_title = re.sub(r'[^\w\-_\. ]', '_', note.title)
-        note_export_path = EXPORT_PATH/ f"{sanitized_title}.md"
+        timestamp = note.created_at.strftime(FILE_TIME_FORMAT)
+        if note.title:
+            title = re.sub(r'[^\w\-_\. ]', '', note.title).replace(' ', '_')
+            note_export_path = EXPORT_PATH / f'{timestamp}__{title}.md'
+        else:
+            note_export_path = EXPORT_PATH / f'{timestamp}.md'
+
         with open(note_export_path, 'w', encoding='utf-8') as file:
             file.write(_note_to_str(note))
         _copy_attachments(note)
@@ -45,7 +52,7 @@ def _load_notes(folder: pathlib.Path) -> list[Note]:
                 notes.append(_load_note(item))
             except Exception as err:
                 print(f"Error processing file `{item}`: `{err}`")
-    return sorted(notes, key=lambda x: x.title.lower(), reverse=True)
+    return sorted(notes, key=lambda x: x.created_at, reverse=True)
 
 
 def _load_note(path: pathlib.Path) -> Note:
@@ -56,36 +63,37 @@ def _load_note(path: pathlib.Path) -> Note:
                 f"Note '{note_obj[JSON_NOTE_TITLE]}' "
                 f"from file '{path}' is trashed"
             )
+        title, created_at = _get_title_and_date(note_obj)
         return Note(
-            title=_get_title(note_obj),
+            title=title,
+            created_at=created_at,
             text=_get_text(note_obj),
             attachments=_get_attachments(note_obj),
-            labels=_get_labels(note_obj)
+            labels=_get_labels(note_obj),
         )
 
 
-def _get_title(note: dict[str, object]) -> str:
+def _get_title_and_date(note: dict[str, object]) -> tuple[str, datetime]:
     usec_to_sec = 1e-6
     timestamp_usec = note['createdTimestampUsec']
     if not isinstance(timestamp_usec, int):
         raise NotImplementedError
-    created_at = datetime.fromtimestamp(timestamp_usec * usec_to_sec).strftime(
-        TITLE_TIME_FORMAT
-    )
+    created_at = datetime.fromtimestamp(timestamp_usec * usec_to_sec)
+    created_at_str = created_at.strftime(TITLE_TIME_FORMAT)
 
     if not note[JSON_NOTE_TITLE]:
-        return created_at
+        title = ''
     else:
-        title = note[JSON_NOTE_TITLE]
+        title = note[JSON_NOTE_TITLE].strip()
         if not isinstance(title, str):
             raise NotImplementedError
 
         if note['isArchived']:
-            return f'{created_at} -- [ARCHIVED] {title}'
+            title = f'[ARCHIVED] {title}'
         elif note['isPinned']:
-            return f'{created_at} -- [PINNED] {title}'
-        else:
-            return f'{created_at} -- {title}'
+            title = f'[PINNED] {title}'
+
+    return title, created_at
 
 
 def _get_text(note: dict[str, object]) -> str:
@@ -137,6 +145,11 @@ def _note_to_str(note: Note) -> str:
 
     Ends note content with a single newline as in common Unix standard.
     """
+    if note.title:
+        title = '# ' + note.title
+    else:
+        title = '# ' + note.created_at.strftime(TITLE_TIME_FORMAT)
+
     attachments_str = '\n'.join(
         f'![{pathlib.Path(attachment).name}](attachments/{attachment})'
         for attachment in note.attachments
